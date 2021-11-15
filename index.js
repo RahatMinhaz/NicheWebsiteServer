cc5131521a32c9607140f6ff96fc290def2a638f
RahatMinhaz <minhaz7591@gmail.com>
2021-11-15 16:56:14 +0600

    tenth commit

--- index.js
+++ index.js
@@ -63,6 +63,15 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.json(result);
         });
+
+        app.put('/users', async(req,res) =>{
+            const user = req.body;
+            const filter = {email:user.email};
+            const options = { upsert: true };
+            const updateDoc = {$set:user};
+            const result = await usersCollection.updateOne(filter,updateDoc,options);
+            res.json(result);
+        })
     }
     finally{
 
